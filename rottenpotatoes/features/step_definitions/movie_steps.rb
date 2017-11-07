bd841ce9c664a9d272c0e45d2ac950eadb40dc99
Kerry <kpeay@uncc.edu>
2017-11-07 22:40:56 +0000

    Fixed step_definitions

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -1,21 +1,11 @@
-#byebug
-
 # Add a declarative step here for populating the DB with movies.
-Given /the following movies exist/ do |movies_table|
-  Movie.create!(movie)
-end
-  
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create!(movie)
   end
-  fail "Unimplemented"
-end
-
-Then /(.*) seed movies should exist/ do | n_seeds |
-  Movie.count.should be n_seeds.to_i
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -23,8 +13,8 @@ end
 
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
-  #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  #  page.content  is the entire content of the page as a string.
+  page.body.should match /#{e1}.*#{e2}/m
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -35,10 +25,32 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  rating_list.split(/,\s*/).each do |rating|
+    rating = "ratings_#{rating}"
+    if uncheck
+      uncheck(rating)
+    else
+      check(rating)
+    end
+  end
 end
 
-Then /I should see all the movies/ do
-  # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+Then /I should (not )?see movies rated: (.*)/ do |should_not, rating_list|
+  rating_list.split(/,\s*/).each do |rating|
+    if should_not
+      assert page.has_no_xpath?('//td', :text => "#{rating}")
+    else
+      assert page.has_xpath?('//td', :text => "#{rating}")
+    end
+  end
+end
+
+Then /I should see (.*) of the movies/ do |quantity|
+  rows = page.all('table#movies tr').count
+  case quantity
+  when 'none'
+    rows.should == 1
+  when 'all'
+    rows.should == 11
+  end
 end
